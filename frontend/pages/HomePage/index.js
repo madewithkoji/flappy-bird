1491b642da797407430c080156dbcc0d97bb3bc9
jones <jones@koji-projects.com>
2019-05-05 02:59:26 +0000

    flappy bird

--- frontend/pages/HomePage/index.js
+++ frontend/pages/HomePage/index.js
@@ -1,24 +1,139 @@
-let img;
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/cXgA1d_E-jY&
+
+
+var bird;
+var pipes = [];
+var birdImage;
 
 function setup() {
-  // make a full screen canvas
   createCanvas(window.innerWidth, window.innerHeight);
-  img = loadImage(Koji.images.mouse); // Load the image
+  bird = new Bird();
+  pipes.push(new Pipe());
+  birdImage = loadImage(Koji.images.bird);
 }
 
 function draw() {
-  // set the background color from the configuration options
   background(Koji.colors.backgroundColor);
 
-  // format our text
-  textSize(24);
-  fill(Koji.colors.textColor);
-  textAlign(CENTER);
+  for (var i = pipes.length-1; i >= 0; i--) {
+    pipes[i].show();
+    pipes[i].update();
 
-  // print out our text
-  text(Koji.strings.content, window.innerWidth / 2, 100);
+    if (pipes[i].hits(bird)) {
+      //console.log("HIT");
+    }
 
-  // setup an image to follow our mouse
-  let imageSize = 100;
-  image(img, mouseX - (imageSize / 2), mouseY - (imageSize / 2), imageSize, imageSize);
+    if (pipes[i].offscreen()) {
+      pipes.splice(i, 1);
+    }
+  }
+
+  bird.update();
+  bird.show();
+
+  if (frameCount % 75 == 0) {
+    pipes.push(new Pipe());
+  }
+}
+
+function keyPressed() {
+  if (key == ' ') {
+    bird.up();
+    //console.log("SPACE");
+  }
+}
+
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/cXgA1d_E-jY&
+
+function Pipe() {
+  this.spacing = 175;
+  this.top = random(height / 6, 3 / 4 * height);
+  this.bottom = height - (this.top + this.spacing);
+  this.x = width;
+  this.w = 80;
+  this.speed = 6;
+
+  this.highlight = false;
+
+  this.hits = function(bird) {
+    if (bird.y < this.top || bird.y > height - this.bottom) {
+      if (bird.x > this.x && bird.x < this.x + this.w) {
+        this.highlight = true;
+        return true;
+      }
+    }
+    this.highlight = false;
+    return false;
+  }
+
+  this.show = function() {
+    fill(Koji.colors.pipeColor);
+    if (this.highlight) {
+      fill(255, 0, 0);
+    }
+    rect(this.x, 0, this.w, this.top);
+    rect(this.x, height-this.bottom, this.w, this.bottom);
+  }
+
+  this.update = function() {
+    this.x -= this.speed;
+  }
+
+  this.offscreen = function() {
+    if (this.x < -this.w) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+}
+
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/cXgA1d_E-jY&
+
+function Bird() {
+  this.y = height/2;
+  this.x = 64;
+
+  this.gravity = 0.7;
+  this.lift = -12;
+  this.velocity = 0;
+
+  this.show = function() {
+    fill(255);
+    // ellipse(this.x, this.y, 32, 32);
+    image(birdImage, this.x, this.y, 32, 32); 
+  }
+
+  this.up = function() {
+    this.velocity += this.lift;
+  }
+
+  this.update = function() {
+    this.velocity += this.gravity;
+    // this.velocity *= 0.9;
+    this.y += this.velocity;
+
+    if (this.y > height) {
+      this.y = height;
+      this.velocity = 0;
+    }
+
+    if (this.y < 0) {
+      this.y = 0;
+      this.velocity = 0;
+    }
+
+  }
+
 }
